742757d1f6099dc542c1d666c326a871a21ebaf6
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2024-10-11 14:06:31 +0800

    feat: set title per page

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,28 +1,59 @@
-import * as React from 'react'
-import { Link, Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import {
+  Link,
+  Outlet,
+  createRootRoute,
+  createRootRouteWithContext,
+  useMatches,
+} from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { useEffect } from "react";
 
-export const Route = createRootRoute({
+interface RootContext {
+  getTitle?: () => string | Promise<string>;
+}
+
+export const Route = createRootRouteWithContext<RootContext>()({
+  beforeLoad: () => ({ getTitle: () => "Portfolio" }),
   component: RootComponent,
-})
+});
 
 function RootComponent() {
+  const matches = useMatches();
+
+  useEffect(() => {
+    // Set document title based on lowest matching route with a title
+    const breadcrumbPromises = [...matches]
+      .reverse()
+      .map((match) => {
+        if (!("getTitle" in match.context)) {
+          return undefined;
+        }
+        const { context } = match;
+        return context.getTitle();
+      })
+      .filter(Boolean);
+    void Promise.all(breadcrumbPromises).then((titles) => {
+      document.title = titles.join(" · ");
+      return titles;
+    });
+  }, [matches]);
+
   return (
     <>
       <div className="p-2 flex gap-2 text-lg">
         <Link
           to="/"
           activeProps={{
-            className: 'font-bold',
+            className: "font-bold",
           }}
           activeOptions={{ exact: true }}
         >
           Home
-        </Link>{' '}
+        </Link>{" "}
         <Link
           to="/about"
           activeProps={{
-            className: 'font-bold',
+            className: "font-bold",
           }}
         >
           About
@@ -32,5 +63,5 @@ function RootComponent() {
       <Outlet />
       <TanStackRouterDevtools position="bottom-right" />
     </>
-  )
+  );
 }
--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -3,6 +3,7 @@ import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/about')({
   component: AboutComponent,
+  beforeLoad: () => ({ getTitle: () => "About" }),
 })
 
 function AboutComponent() {
