8ae4dfa1e00f1c43097af9b8115534b03d5019b5
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2024-10-16 12:40:22 +0800

    chore: refactor navbar

--- src/routes/_navbar/blogs/index.tsx
+++ src/routes/_navbar/blogs/index.tsx
@@ -1,6 +1,11 @@
-import { createFileRoute } from '@tanstack/react-router'
+import Underconstruction from "@/components/underconstruction";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/_navbar/blogs/')({
-  component: () => <div>Hello /_navbar/blog/!</div>,
-  beforeLoad: () => ({ getTitle: () => 'Blogs' }),
-})
+export const Route = createFileRoute("/_navbar/blogs/")({
+  component: BlogsComponent,
+  beforeLoad: () => ({ getTitle: () => "Blogs" }),
+});
+
+function BlogsComponent() {
+  return <Underconstruction />;
+}
