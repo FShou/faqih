b86fe14c6f894f0ef2cdad245bbea2d621c09e3d
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2024-10-17 12:53:55 +0800

    feat: project detail page and not use the navbar layout

--- src/routes/_navbar/projects.tsx
+++ src/routes/_navbar/projects.tsx
@@ -1,15 +1,11 @@
 import Underconstruction from '@/components/underconstruction'
 import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute('/_navbar/projects/')({
+export const Route = createFileRoute('/_navbar/projects')({
   component: ProjectsComponent,
   beforeLoad: () => ({ getTitle: () => 'Projects' }),
 })
 
-
-function ProjectsComponent(){
-
-  return (
-  <Underconstruction />
-  )
+function ProjectsComponent() {
+  return <Underconstruction />
 }
