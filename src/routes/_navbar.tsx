f1add3bd5e5f4d6afd29a99f54313fb8df024500
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2024-10-17 12:06:21 +0800

    fix: reload page go to home

--- src/routes/_navbar.tsx
+++ src/routes/_navbar.tsx
@@ -1,5 +1,5 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { createFileRoute, Outlet, useNavigate } from "@tanstack/react-router";
+import { createFileRoute, Outlet,  useNavigate, useRouterState } from "@tanstack/react-router";
 import { useEffect } from "react";
 
 export const Route = createFileRoute("/_navbar")({
@@ -8,23 +8,35 @@ export const Route = createFileRoute("/_navbar")({
 });
 
 function Navbar() {
+
+  const pathname = useRouterState().location.pathname
+    
+  
   const navItems = [
     { title: "Home", route: "/" },
     { title: "About", route: "/about" },
     { title: "Projects", route: "/projects" },
     { title: "Blog", route: "/blogs" },
   ];
+
+  const defaulVal = navItems
+    .filter(it => it.route.includes(pathname))
+    .at(0)
+    ?.title 
+    || ""
+
+
   return (
     <div className="flex flex-col-reverse md:flex-col  w-svw h-svh">
       <div className="w-full h-12 pt-2 flex justify-center items-center">
-        <Tabs defaultValue="Home">
+        <Tabs defaultValue={defaulVal}>
           <TabsList>
-            {navItems.map((it) => (
-              <TabsTrigger value={it.title}>{it.title}</TabsTrigger>
+            {navItems.map((it,idx) => (
+              <TabsTrigger key={it.title+idx} value={it.title}>{it.title}</TabsTrigger>
             ))}
           </TabsList>
-          {navItems.map((it) => (
-            <TabsContent value={it.title}>
+          {navItems.map((it,idx) => (
+            <TabsContent key={it.title+idx}value={it.title}>
               <Navigate to={it.route} />
             </TabsContent>
           ))}
