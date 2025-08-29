0e32f7ab71d660139c663c7425f318446ecc574f
Achmad Faqih Suyudi <faqihsuyudi2020@gmail.com>
2025-08-29 21:58:58 +0800

    dah lah mending vite + hono tuh wakwak

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,4 +1,3 @@
-import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
@@ -6,10 +5,10 @@ import path from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [tailwindcss(), tanstackRouter({}), react(), cloudflare()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
+  plugins: [tailwindcss(), tanstackRouter({}), react(),],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
